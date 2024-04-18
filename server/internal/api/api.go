0a5cc190fa2b771b7eb1eb241574d6f39fb55818
zerodoctor <zerulus@gmail.com>
2024-04-18 00:15:02 -0500

    add register organization endpoint

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zerodoctor/shawarma/internal/db"
+)
+
+type API struct {
+	db db.DB
+}
+
+func InternalError(ctx *gin.Context, err error) {
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": err.Error(),
+	})
+}
--- server/internal/api/git.go
+++ server/internal/api/git.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zerodoctor/shawarma/internal/model"
+)
+
+func (api *API) RegisterOrganization(ctx *gin.Context) {
+	var org model.Organization
+	if err := ctx.BindJSON(org); err != nil {
+		InternalError(ctx, err)
+		return
+	}
+
+	if _, err := api.db.InsertOrganization(org); err != nil {
+		InternalError(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusAccepted, org)
+}
